10b073f9690609970f31e2dc240c3fa5e3706436
Alan Cunningham <ajc.alan@gmail.com>
2024-05-28 16:19:27 +0100

    Add a flask server which allows easy updating of the desired Agile threshold

---
+++ flask_server/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+price_threshold = 8
+
+
+@app.route("/", methods=["GET", "POST"])
+def set_price_threshold():
+    """
+    Interface to set the price threshold
+    """
+    global price_threshold
+    if request.method == "POST":
+        price_threshold = request.form["price"]
+        print(f"New price: {price_threshold}")
+    return render_template("index.html", price_threshold=price_threshold)
+
+
+@app.route("/price")
+def get_price_threshold():
+    """
+    Returns JSON response of the currently set price threshold
+    """
+    response = {"threshold": price_threshold}
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
